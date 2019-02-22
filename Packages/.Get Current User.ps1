fa38963b8b80e3afffb41c040b72635276814558
DeWitt <DeWitt-L@mssu.edu>
2019-02-22 10:22:07 -0600

    initial commit

---
+++ Packages/.Get Current User.ps1
@@ -0,0 +1,34 @@
+# Exit codes
+$completed   =  0,"."
+$connected   = -1,"."
+$copying     = -2,"."
+$installing  = -3,"."
+$verifying   = -4,"."
+$customMsg   = 1
+$copyErr     = 2
+$installErr  = 3
+$verErr      = 4,"."
+
+Start-Sleep 1
+Write-Output $connected
+Start-Sleep 1
+
+$currentUser = $null
+try {$currentUser = (Get-WMIObject -class Win32_ComputerSystem -ErrorAction Stop).username }
+catch {Write-Output $customMsg, "Device is online`nUnable to check user/state status"; return}
+
+if ($currentUser)
+{
+    try
+    {
+        Get-Process logonui -ErrorAction Stop
+        Write-Output $customMsg, "Device is online`nCurrent user : $currentUser`nDevice state : Locked"
+        return
+    }
+    catch {Write-Output $customMsg, "Device is online`nCurrent user : $currentUser`nDevice state : Unlocked";return}
+}
+else {Write-Output $customMsg, "Device is online`nCurrent user : None`nDevice state : Locked"; return}
+
+
+Start-Sleep 1
+Write-Output $customMsg, "Device is online`nUnable to check user/state status"
